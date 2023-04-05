55c6c64e0f561c54e6fee728e2dd66234e4287b9
BuseMutluoglu <busemutluoglu@gmail.com>
2023-04-05 09:52:31 +0300

    s15g1 tamamlandı

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -6,9 +6,6 @@
     "message": "Geçemezsiniz!"
   }
 */
-function sinirli() {
-
-}
 
 /*
   req.body de verilen username halihazırda veritabanında varsa
@@ -18,10 +15,39 @@ function sinirli() {
     "message": "Username kullaniliyor"
   }
 */
-function usernameBostami() {
+const model = require("../users/users-model");
+const bcrypt = require("bcryptjs");
 
+function sinirli(req, res, next) {
+  try {
+    if (req.session && req.session.user_id) {
+      next();
+    } else {
+      next({
+        status: 401,
+        message: "Geçemezsiniz!",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
 }
-
+async function usernameBostami(req, res, next) {
+  try {
+    const userExist = await model.goreBul({ username: req.body.username });
+    if (userExist && userExist.length) {
+      next({
+        status: 422,
+        message: "Username kullaniliyor",
+      });
+    } else {
+      req.body.password = bcrypt.hashSync(req.body.password);
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+}
 /*
   req.body de verilen username veritabanında yoksa
 
@@ -30,8 +56,35 @@ function usernameBostami() {
     "message": "Geçersiz kriter"
   }
 */
-function usernameVarmi() {
-
+async function usernameVarmi(req, res, next) {
+  try {
+    const userListFromUserName = await model.goreBul({
+      username: req.body.username,
+    });
+    let userExist;
+    for (let i = 0; i < userListFromUserName.length; i++) {
+      const dbUser = userListFromUserName[i];
+      let isPasswordMatched = await bcrypt.compare(
+        req.body.password,
+        dbUser.password
+      );
+      if (isPasswordMatched && dbUser.username == req.body.username) {
+        userExist = dbUser;
+        break;
+      }
+    }
+    if (!userExist) {
+      next({
+        status: 401,
+        message: "Geçersiz kriter",
+      });
+    } else {
+      req.user = userExist;
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 /*
@@ -42,8 +95,20 @@ function usernameVarmi() {
     "message": "Şifre 3 karakterden fazla olmalı"
   }
 */
-function sifreGecerlimi() {
-
+function sifreGecerlimi(req, res, next) {
+  try {
+    if (!req.body.password || req.body.password.length < 3) {
+      next({
+        status: 422,
+        message: "Şifre 3 karakterden fazla olmalı",
+      });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 // Diğer modüllerde kullanılabilmesi için fonksiyonları "exports" nesnesine eklemeyi unutmayın.
+module.exports = { sifreGecerlimi, usernameBostami, usernameVarmi, sinirli };
--- api/server.js
+++ api/server.js
@@ -1,7 +1,3 @@
-const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
-
 /**
   Kullanıcı oturumlarını desteklemek için `express-session` paketini kullanın!
   Kullanıcıların gizliliğini ihlal etmemek için, kullanıcılar giriş yapana kadar onlara cookie göndermeyin. 
@@ -15,21 +11,54 @@ const cors = require("cors");
   veya "connect-session-knex" gibi bir oturum deposu kullanabilirsiniz.
  */
 
-const server = express();
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const userModel = require("./users/users-model");
 
+const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+const session = require("express-session");
+const SessionStore = require("connect-session-knex")(session);
+
+const userRouter = require("./users/users-router");
+const authRouter = require("./auth/auth-router");
+
+server.use(
+  session({
+    name: "cikolatacips",
+    secret: "secret_cikolatacips",
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+      secure: false,
+    },
+    store: new SessionStore({
+      knex: require("../data/db-config"),
+      tableName: "sessions",
+      sidFieldName: "sid",
+      createTable: true,
+      clearInterval: 1000 * 60 * 60,
+    }),
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+server.use("/api/users", userRouter);
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use((err, req, res, next) => {
+  // eslint-disable-line
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
   });
 });
-
 module.exports = server;
